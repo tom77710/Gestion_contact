a7b6291a9e556b523bd2a3c00519bcecf0aa6f05
Thomas MENIN <menin.thomas.pro@gmail.com>
2024-12-19 14:20:02 +0100

    Suite_code_1

--- main.cpp
+++ main.cpp
@@ -19,3 +19,41 @@ void mettreAJourContact(std::vector<Contact>& contacts);
 void supprimerContact(std::vector<Contact>& contacts);
 void sauvegarderContacts(const std::vector<Contact>& contacts, const std::string& fichier);
 void chargerContacts(std::vector<Contact>& contacts, const std::string& fichier);
+
+int main() {
+    std::vector<Contact> contacts;
+    std::string fichier = "contacts.txt";
+
+    // Charger les contacts depuis le fichier
+    chargerContacts(contacts, fichier);
+
+    int choix;
+    do {
+        afficherMenu();
+        std::cin >> choix;
+        std::cin.ignore(); // Pour nettoyer le tampon d'entrée
+
+        switch (choix) {
+            case 1:
+                ajouterContact(contacts);
+                break;
+            case 2:
+                afficherContacts(contacts);
+                break;
+            case 3:
+                mettreAJourContact(contacts);
+                break;
+            case 4:
+                supprimerContact(contacts);
+                break;
+            case 5:
+                sauvegarderContacts(contacts, fichier);
+                std::cout << "Contacts sauvegardes. Au revoir !\n";
+                break;
+            default:
+                std::cout << "Choix invalide, veuillez reessayer.\n";
+        }
+    } while (choix != 5);
+
+    return 0;
+}
